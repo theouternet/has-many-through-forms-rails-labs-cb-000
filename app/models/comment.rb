8298265229f274e65ac0460e84c46078ec78ffd1
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-01 21:32:38 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
- accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
 
   def user_attributes=(user_attributes)
     self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
   end
 
 
-end
 end
