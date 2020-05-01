9a5b3bd3697e1844aad1e2a890bce246da22c417
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-01 21:33:17 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
+
+
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      unless category_attribute["name"].blank?
+        category = Category.find_or_create_by(category_attribute)
+        self.post_categories.build(category: category) unless self.categories.include?(category)
+      end
+    end
+  end
+
 
 end
