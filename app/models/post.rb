44128e6768a9f577c8532e482db848c1431ac790
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-01 21:20:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,20 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  accepts_nested_attributes_for :categories
+accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
 
+
+  # the below overrides the built-in setter of the "accepts..." to prevent dupes
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+      unless category_attribute["name"].blank?
+        category = Category.find_or_create_by(category_attribute)
+        self.post_categories.build(category: category) unless self.categories.include?(category)
+      end
     end
   end
-end
 
+
+end
+end
